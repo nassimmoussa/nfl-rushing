65207d3db263d9e72a612fafc19fd04b9efd31bb
Nassim Moussa <nassimmoussa1993@gmail.com>
2021-05-14 15:58:33 -0300

    phoenix project setup

---
+++ config/config.exs
@@ -0,0 +1,28 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :nfl_rushing, NflRushingWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "i80I3HPmeyB8UWPtOGfMMoarXoM6UXzDhWyyIAoTl1g293Gmd3lEuY6aGfWh+CVl",
+  render_errors: [view: NflRushingWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: NflRushing.PubSub,
+  live_view: [signing_salt: "rM+v0tkq"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
